4c78e406b7bc9be863c6506e8c6050b14760c5a8
Odin Dutton <odindutton@gmail.com>
2021-11-20 08:50:05 +1100

    Add "Serves" to each recipe

--- src/recipes.ts
+++ src/recipes.ts
@@ -28,6 +28,7 @@ export const recipes: Recipe[] = [
     name: "Vesper Martini",
     directions: "Combine ingredients, shake well, and strain into a chilled cocktail glass. Garnish with a large piece of lemon peel.",
     ingredients: [
+      { name: "Serves", amount: 1 },
       { name: "Gin", amount: 3 },
       { name: "Vodka", amount: 1 },
       { name: "Lillet Blanc or Dry Vermouth", amount: 0.5 },
@@ -37,6 +38,7 @@ export const recipes: Recipe[] = [
     name: "Negroni",
     directions: "Combine ingredients, stir, then pour into a glass with a large piece of ice. Garnish with a slice of orange and a dash of bitters.",
     ingredients: [
+      { name: "Serves", amount: 1 },
       { name: "Gin", amount: 1 },
       { name: "Sweet Vermouth", amount: 1 },
       { name: "Campari", amount: 1 },
@@ -46,6 +48,7 @@ export const recipes: Recipe[] = [
     name: "Mezcal Margarita",
     directions: "Combine ingredients, shake well, and strain into a chilled cocktail glass.",
     ingredients: [
+      { name: "Serves", amount: 1 },
       { name: "Mezcal", amount: 2 },
       { name: "Lime Juice", amount: 1.5 },
       { name: "Cointreau", amount: 1 },
@@ -56,6 +59,7 @@ export const recipes: Recipe[] = [
     name: "Tommy's Margarita",
     directions: "Combine, shake well, pour into a salt-rimmed glass with ice.",
     ingredients: [
+      { name: "Serves", amount: 1 },
       { name: "Tequila", amount: 2 },
       { name: "Lime juice", amount: 1 },
       { name: "Agave syrup", amount: 0.5 },
@@ -65,6 +69,7 @@ export const recipes: Recipe[] = [
     name: "Aviation",
     directions: "Combine ingredients, shake well, and strain into a chilled cocktail glass. Garnish with a flamed lemon peel.",
     ingredients: [
+      { name: "Serves", amount: 1 },
       { name: "Gin", amount: 2 },
       { name: "Maraschino liqueur", amount: 0.25 },
       { name: "Crème de violette liqueur", amount: 0.25 },
@@ -75,6 +80,7 @@ export const recipes: Recipe[] = [
     name: "Gin & It",
     directions: "Combine ingredients and stir with ice. Strain into a chilled glass and garnish with a twist of orange and a dash of orange bitters.",
     ingredients: [
+      { name: "Serves", amount: 1 },
       { name: "Gin", amount: 2 },
       { name: "Sweet vermouth", amount: 1.5 },
     ],
